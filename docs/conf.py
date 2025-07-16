4d2c72d17870ff931b267009945af7b9108c2217
Michael E. Cotterell <mepcotterell@gmail.com>
2025-07-16 09:54:25 -0400

    added docs

---
+++ docs/conf.py
@@ -0,0 +1,30 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "cs1302-sphinx"
+copyright = "2025, Michael E. Cotterell"
+author = "Michael E. Cotterell"
+release = "0.1.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "cs1302_sphinx",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_title = "cs1302-sphinx"
+html_theme = "cs1302_base"
+html_static_path = ["_static"]
